66373ef4c7be5f2611571d910bea0bf8d98f7a9a
2oo1s <90839206+2oo1s@users.noreply.github.com>
2023-08-03 22:22:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 155.70 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,21 +4,25 @@ class Solution {
 	public String solution(String[] participant, String[] completion) {
 		String answer = "";
 
-		Map<String, Integer> map = new HashMap<>();
+		// 선수 이름을 key값, participant 배열에 선수 이름이 몇번 있는지를 value값으로 
+		Map<String, Integer> runner = new HashMap<>();
 
 		for (String name : participant) {
-			if (map.get(name) == null)
-				map.put(name, 1);
+			if (runner.get(name) == null)
+				runner.put(name, 1);
+			// 이름이 1번 이상 나온 선수면 value값 +1 
 			else
-				map.put(name, map.get(name) + 1);
+				runner.put(name, runner.get(name) + 1);
 		}
 
+		// completion에 있는 선수를 runner Map에서 찾아서 value값 -1 
 		for (String name : completion) {
-			map.put(name, map.get(name) - 1);
+			runner.put(name, runner.get(name) - 1);
 		}
 
-		for (String name : map.keySet()) {
-			if (map.get(name) != 0) {
+		// value값이 0이 아닌 선수가 완주 못한 선수이므로 answer에 저장 
+		for (String name : runner.keySet()) {
+			if (runner.get(name) != 0) {
 				answer = name;
 				break;
 			}
