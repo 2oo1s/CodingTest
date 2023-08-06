6077396207cfe2c2b13f22159fa21bc2a390c797
2oo1s <90839206+2oo1s@users.noreply.github.com>
2023-08-06 14:39:32 +0900

    [level 1] Title: 모의고사, Time: 0.75 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -4,17 +4,15 @@ class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
         // 3인방이 문제 찍는 방법 저장
-        int[] first = {1,2,3,4,5};
-        int[] second = {2,1,2,3,2,4,2,5};
-        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        int[][] student = {{1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}};
         // 삼인방 맞은 문제 카운트 
         int[] cnt = new int[3];
         for(int i = 0; i < answers.length; i++){
-            if(answers[i] == first[i % first.length])
+            if(answers[i] == student[0][i % student[0].length])
                 cnt[0]++;
-            if(answers[i] == second[i % second.length])
+            if(answers[i] == student[1][i % student[1].length])
                 cnt[1]++;
-            if(answers[i] == third[i % third.length])
+            if(answers[i] == student[2][i % student[2].length])
                 cnt[2]++;
         }
         // 가장 많이 맞춘게 몇 문제인지
