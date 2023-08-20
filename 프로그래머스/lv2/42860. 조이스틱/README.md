70c74d848a6df2252ab3a1db15ab898d0de10b3d
2oo1s <90839206+2oo1s@users.noreply.github.com>
2023-08-20 15:59:34 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/README.md
+++ 프로그래머스/lv2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 0.05 ms
+메모리: 73.4 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 class Solution {
-	public int solution(String name) {
-		int answer = 0;
 
-		int move = name.length() - 1; // 오른쪽으로 움직이는 횟수
-		for (int i = 0; i < name.length(); i++) {
-			// A와 Z 중에 더 가까운 쪽으로부터 접근
-			answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-			int next = i + 1;
-			while (next < name.length() && name.charAt(next) == 'A') {
-				next++;
-			}
-			move = Math.min(move, i+(name.length()-next)+Math.min(i,name.length()-next));
-		}
-		return answer + move;
-	}
+  public int solution(String name) {
+    int answer = 0;
+    int len = name.length();
+    int move = len - 1; // 오른쪽으로 움직이는 횟수
+    for (int i = 0; i < len; i++) {
+      // A와 Z 중에 더 가까운 쪽으로부터 접근
+      answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+      int next = i + 1;
+      while (next < len && name.charAt(next) == 'A') {
+        next++;
+      }
+      move = Math.min(move, i + (len - next) + Math.min(i, len - next));
+    }
+    return answer + move;
+  }
 }
