61cfc37211496b03da0f6198b85c54e6667f2d4c
2oo1s <90839206+2oo1s@users.noreply.github.com>
2023-09-03 00:20:59 +0900

    [Gold IV] Title: 타일 채우기, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+
+		if (N % 2 != 0) {
+			System.out.println(0);
+			return;
+		}
+		
+		dp[0] = 1; // 크기가 0일 때,
+		dp[1] = 0; // 못 채우는 경
+		dp[2] = 3; // 3 * 2 채우는 경우
+
+		// N 짝수일 때만 확인
+		for (int i = 4; i <= N; i += 2) {
+			dp[i] = dp[i - 2] * dp[2];
+			for (int j = 0; j < i - 2; j += 2) {
+				dp[i] = dp[i] + dp[j] * 2;
+			}
+		}
+		System.out.println(dp[N]);
+	}
+}
