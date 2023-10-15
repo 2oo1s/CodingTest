23683e0123e12e1a26dc09b704f2004d41492dc8
2oo1s <90839206+2oo1s@users.noreply.github.com>
2023-10-15 14:12:55 +0900

    [Gold II] Title: 후위 표기식, Time: 156 ms, Memory: 16252 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		String answer = "";
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			char temp = str.charAt(i);
+
+			switch (temp) {
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				while (!stack.isEmpty() && priority(stack.peek()) >= priority(temp))
+					answer += stack.pop();
+				stack.add(temp);
+
+				break;
+			case '(':
+				stack.add(temp);
+
+				break;
+			case ')':
+				while (!stack.isEmpty() && stack.peek() != '(')
+					answer += stack.pop();
+				stack.pop(); // '(' 제거
+
+				break;
+			default:
+				answer += temp;
+			}
+		}
+
+		while (!stack.isEmpty())
+			answer += stack.pop();
+
+		System.out.println(answer);
+	}
+
+	public static int priority(char operator) {
+		if (operator == '*' || operator == '/')
+			return 2;
+		else if (operator == '+' || operator == '-')
+			return 1;
+		else if (operator == '(' || operator == ')')
+			return 0;
+		else
+			return -1;
+	}
+
+}
