2d92cd8ba0a82f2e40d395f13c4995d6a8454a1a
2oo1s <90839206+2oo1s@users.noreply.github.com>
2023-10-20 11:08:28 +0900

    [Silver II] Title: 주식, Time: 1112 ms, Memory: 317912 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -6,24 +6,24 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine()); // 테케 수
-		long[] profit = new long[T]; // 테케별 이익
+
+		int T = Integer.parseInt(br.readLine());
+		long[] profit = new long[T];
 
 		for (int tc = 0; tc < T; tc++) {
-			int N = Integer.parseInt(br.readLine()); // 날의 수
-			int[] stocks = new int[N];
-			int max = 0;
+			int N = Integer.parseInt(br.readLine());
+			int[] stocks = new int[N]; // 날 별 주식 가격 저장
 
 			st = new StringTokenizer(br.readLine(), " ");
-			for (int i = 0; i < N; i++) {
+			for (int i = 0; i < N; i++)
 				stocks[i] = Integer.parseInt(st.nextToken());
-			}
 
+			int max = 0;
 			for (int i = N - 1; i >= 0; i--) {
 				if (stocks[i] > max)
 					max = stocks[i];
 				else
-					profit[tc] += (max - stocks[i]);
+					profit[tc] += max - stocks[i];
 			}
 		}
 		for (long p : profit)
