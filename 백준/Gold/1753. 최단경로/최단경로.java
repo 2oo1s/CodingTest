2a5a09cbab9011d62e18bc520d1bebbbeae54157
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-01-21 21:12:22 +0900

    [Gold IV] Title: 최단경로, Time: 944 ms, Memory: 109600 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,65 +1,68 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int V, E, K; // 정점, 간선, 시작점
-    static ArrayList<Edge> adjL[];  // 방향 그래프 저장
-    static int dist[];  // 시작점으로부터 각 노드애 도달하는 거리 저장
-    static boolean visited[];
-    static PriorityQueue<Edge> q = new PriorityQueue<>();
+    static int V, E, K;
+    static ArrayList<Edge> graph[];
+    static boolean[] visited;
+    static int[] dist;
+    static PriorityQueue<Edge> q;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
 
-        adjL = new ArrayList[V + 1];
-        dist = new int[V + 1];
+        graph = new ArrayList[V + 1];
         visited = new boolean[V + 1];
+        dist = new int[V + 1];
+        q = new PriorityQueue<>();
 
-        for (int i = 1; i <= V; i++)
-            adjL[i] = new ArrayList<Edge>();
+       for (int i = 0; i <= V; i++)
+            graph[i] = new ArrayList<>();
 
-        for (int i = 1; i <= V; i++)
+        for (int i = 0; i <= V; i++)
             dist[i] = Integer.MAX_VALUE;
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine(), " ");
 
-            int u = Integer.parseInt(st.nextToken());   // start
-            int v = Integer.parseInt(st.nextToken());   // end
-            int w = Integer.parseInt(st.nextToken());   // 가중치
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            adjL[u].add(new Edge(v, w));
+            graph[u].add(new Edge(v, w));
         }
-
         q.add(new Edge(K, 0));
         dist[K] = 0;
 
         while (!q.isEmpty()) {
             Edge cur = q.poll();
-            int cur_v = cur.vertex;
+            int curVertex = cur.vertex;
 
-            if (visited[cur_v])
-                continue;
+            if (!visited[curVertex]) {
+                visited[curVertex] = true;
+                for (int i = 0; i < graph[curVertex].size(); i++) {
+                    Edge temp = graph[curVertex].get(i);
 
-            visited[cur_v] = true;
+                    int nextV = temp.vertex;
+                    int value = temp.value;
 
-            for (int i = 0; i < adjL[cur_v].size(); i++) {
-                Edge temp = adjL[cur_v].get(i); // 순서대로 연결된 노드들 탐색
-                int next = temp.vertex;
-                int value = temp.value;
-
-                if (dist[next] > dist[cur_v] + value) { // 더 적은 가중치로 도달 가능한 경우, 갱신
-                    dist[next] = dist[cur_v] + value;
-                    q.add(new Edge(next, dist[next]));
+                    if (dist[nextV] > dist[curVertex] + value) {
+                        dist[nextV] = dist[curVertex] + value;
+                        q.add(new Edge(nextV, dist[nextV]));
+                    }
                 }
             }
         }
+
         for (int i = 1; i <= V; i++) {
             if (visited[i])
                 System.out.println(dist[i]);
@@ -69,14 +72,16 @@ public class Main {
     }
 
     static class Edge implements Comparable<Edge> {
-        int vertex, value;
-        public Edge(int v, int w) {
-            this.vertex = v;
-            this.value = w;
+        int vertex, value;  // 연결된 정점, 가중치 저장
+
+        public Edge(int vertex, int value) {
+            this.vertex = vertex;
+            this.value = value;
         }
+
         @Override
-        public int compareTo(Edge o) {
-            return value - o.value;
+        public int compareTo(Edge e) {
+            return this.value - e.value;
         }
     }
 }
