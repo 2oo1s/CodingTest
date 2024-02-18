206f2cf6bfee064e7262dd8b2614a78e7107ccae
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-02-18 22:45:56 +0900

    [Gold IV] Title: 문자열 폭발, Time: 564 ms, Memory: 61820 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String target = br.readLine();
+        StringBuilder answer = new StringBuilder();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+
+            if (stack.size() >= target.length()) {
+                boolean flag = true;
+                
+                for (int j = 0; j < target.length(); j++) {
+                    if (stack.get(stack.size() - target.length() + j) != target.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    for (int j = 0; j < target.length(); j++)
+                        stack.pop();
+                }
+            }
+        }
+
+        for (Character c : stack)
+            answer.append(c);
+
+
+        if (answer.length() == 0)
+            System.out.println("FRULA");
+        else
+            System.out.println(answer);
+    }
+}
