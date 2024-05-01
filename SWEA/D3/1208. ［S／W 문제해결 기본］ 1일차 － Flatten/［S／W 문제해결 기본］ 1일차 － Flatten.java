76d57d518200967777fcdaf8778ab2cb733d0c4f
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-05-01 17:56:38 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 141 ms, Memory: 25,788 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int dump = Integer.parseInt(br.readLine());
+			PriorityQueue pq1 = new PriorityQueue<>();
+			PriorityQueue pq2 = new PriorityQueue<>(Collections.reverseOrder());
+
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < 100; i++) {
+				int element = Integer.parseInt(st.nextToken());
+				pq1.add(element);
+				pq2.add(element);
+			}
+
+			for (int i = 0; i < dump; i++) {
+				int max = (int) pq2.poll();
+				int min = (int) pq1.poll();
+
+				max -= 1;
+				min += 1;
+				pq2.add(max);
+				pq1.add(min);
+			}
+			System.out.println("#" + test_case + " " + ((int) pq2.poll() - (int) pq1.poll()));
+		}
+	}
+}
