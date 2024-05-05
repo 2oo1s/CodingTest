983b529185fb8d476ea5e1f86d5fff46631c9062
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-05-05 14:32:54 +0900

    [D2] Title: 초심자의 회문 검사, Time: 104 ms, Memory: 25,192 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int tc = Integer.parseInt(br.readLine());
+
+		for (int test_case = 1; test_case <= tc; test_case++) {
+			boolean flag = true;
+			String str = br.readLine();
+			char[] arr = str.toCharArray();
+
+			int s = 0, e = str.length() - 1;
+
+			while (s < e) {
+				if (arr[s] == arr[e]) {
+					s++;
+					e--;
+				} else {
+					flag = false;
+					break;
+				}
+			}
+			if (flag)
+				System.out.println("#" + test_case + " 1");
+			else
+				System.out.println("#" + test_case + " 0");
+		}
+	}
+}
