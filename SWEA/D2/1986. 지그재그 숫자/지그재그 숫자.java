21b810b6b418a6de129176b5a13118aed27187dc
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-05-06 16:33:08 +0900

    [D2] Title: 지그재그 숫자, Time: 104 ms, Memory: 26,220 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int tc = Integer.parseInt(br.readLine());
+
+		for (int test_case = 1; test_case <= tc; test_case++) {
+			int n = Integer.parseInt(br.readLine());
+			int answer = 0;
+
+			for (int i = 1; i <= n; i++) {
+				if (i % 2 == 1)
+					answer += i;
+				else
+					answer -= i;
+			}
+			System.out.println("#" + test_case + " " + answer);
+		}
+	}
+}
