3c69171ee23b276994df74181ed518a7c0375333
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-09-02 17:48:45 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,52 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static int pair;
-	static ArrayList<Integer> com[];
-	static boolean visited[];
-	static int ans = 0; // 바이러스 걸리는 컴퓨터 개수
+    static ArrayList<Integer>[] list;
+    static boolean[] visited;
+    static int ans = 0;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine()); // 컴퓨터 개수
-		pair = Integer.parseInt(br.readLine()); // 연결 정보
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		com = new ArrayList[N + 1]; // 컴퓨터 개수만큼 공간 할당
-		visited = new boolean[N + 1];
-		for (int i = 0; i < N + 1; i++)
-			com[i] = new ArrayList<Integer>(); // 각 공간마다 ArrayList 만들어주기
+        int n = Integer.parseInt(br.readLine());
+        int pair = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < pair; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			com[s].add(e);
-			com[e].add(s);
-		}
-		bfs(1); // 1번 노드터 탐색
+        list = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
 
-		System.out.println(ans);
-		// System.out.println(Arrays.deepToString(com));
-	}
+        for (int i = 0; i <= n; i++)
+            list[i] = new ArrayList<>();
 
-	public static void bfs(int n) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(n);
-		visited[n] = true;
+        for (int i = 0; i < pair; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-		while (!q.isEmpty()) {
-			int temp = q.poll();
-			for (int i : com[temp]) {
-				// 탐색 안 한 컴퓨터
-				if (!visited[i]) {
-					ans += 1;
-					q.add(i);
-					visited[i] = true;
-				}
-			}
-		}
-	}
+            list[a].add(b);
+            list[b].add(a);
+        }
+        // System.out.println(Arrays.deepToString(list));
 
+        visited[1] = true;
+        dfs(1);
+        System.out.println(ans);
+    }
+
+    public static void dfs(int v) {
+        visited[v] = true;
+
+        for (int i : list[v]) {
+            if (!visited[i]) {
+                ans += 1;
+                visited[i] = true;
+                dfs(i);
+            }
+        }
+    }
 }
