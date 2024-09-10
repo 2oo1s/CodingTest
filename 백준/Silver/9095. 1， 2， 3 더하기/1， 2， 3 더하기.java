e520554b93b655aea482b44d19792b02e9792fce
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-09-10 21:54:49 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,16 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int tc = Integer.parseInt(br.readLine());
+
         int[] dp = new int[11];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
+
+        dp[1] = 1;  // 1 = 1
+        dp[2] = 2;  // 2 = 1 + 1 = 2
+        dp[3] = 4;  // 3 = 1 + 1 + 1 = 1 + 2 = 2 + 1 = 3
+
+        /*
+        dp[4] = 1 + 3 -> 3을 만드는 방법은 dp[3]만큼 가능
+              = 2 + 2 -> 2를   "    "   dp[2]만큼 가능
+              = 3 + 1 -> 1을   "    "   dp[1]만큼 가능
+         */
         for (int i = 4; i < 11; i++)
             dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
 
